5e3180235cde2f536770c25e1e4cdbcc97f0f8c5
akursus <ak_ursus@list.ru>
2022-11-13 18:25:57 +0300

    Lesson 7.11.22 Hw 2 Task 5

--- src/Main.java
+++ src/Main.java
@@ -18,12 +18,19 @@ public class Main {
         System.out.println("dog = "+dog);
         System.out.println("cat = "+cat);
         System.out.println("paper = "+paper);
-        var friend = 19;
+        var friend = 19.0;
         System.out.println("friend = " +friend);
         friend *= 2;
         System.out.println("friend = " +friend);
         friend /= 7;
         System.out.println("friend = " +friend);
-
+        var frog = 3.5;
+        System.out.println("frog = " +frog);
+        frog *= 10;
+        System.out.println("frog = " +frog);
+        frog /= 3.5;
+        System.out.println("frog = " +frog);
+        frog += 4;
+        System.out.println("frog = " +frog);
     }
 }
