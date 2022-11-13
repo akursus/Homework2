7657eee17e2f4d08b72bdc3ecabd0343eaef52f8
akursus <ak_ursus@list.ru>
2022-11-13 20:09:40 +0300

    Lesson 7.11.22 Hw 2 Task 6

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,28 @@ public class Main {
         System.out.println("dog = "+dog);
         System.out.println("cat = "+cat);
         System.out.println("paper = "+paper);
+
         dog += 4;
         cat += 4;
         paper += 4;
         System.out.println("dog = "+dog);
         System.out.println("cat = "+cat);
         System.out.println("paper = "+paper);
+
         dog -= 3.5;
         cat -= 1.6;
         paper -= 7639;
         System.out.println("dog = "+dog);
         System.out.println("cat = "+cat);
         System.out.println("paper = "+paper);
+
         var friend = 19.0;
         System.out.println("friend = " +friend);
         friend *= 2;
         System.out.println("friend = " +friend);
         friend /= 7;
         System.out.println("friend = " +friend);
+
         var frog = 3.5;
         System.out.println("frog = " +frog);
         frog *= 10;
@@ -32,5 +36,14 @@ public class Main {
         System.out.println("frog = " +frog);
         frog += 4;
         System.out.println("frog = " +frog);
+
+        var WeightFirstBoxer = 78.2;
+        var WeightSecondBoxer = 82.7;
+        var TotalWeigtBoxer = WeightFirstBoxer + WeightSecondBoxer;
+        var WeightDifferenceBoxer = TotalWeigtBoxer % WeightFirstBoxer;
+        System.out.println("Общий вес двух бойцов " +TotalWeigtBoxer +" кг");
+        System.out.println("Разница между весами бойцов " +WeightDifferenceBoxer +" кг");
+
+
     }
 }
